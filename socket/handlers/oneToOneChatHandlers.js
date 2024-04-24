d8891e47b33df8a72a1c49749a8159f87db37fa0
Manish kumar <manojgeetparmar@gmail.com>
2024-04-24 11:16:03 +0530

    New : Socket event- One to one message, send recieve

--- socket/handlers/oneToOneChatHandlers.js
+++ socket/handlers/oneToOneChatHandlers.js
@@ -1,4 +1,5 @@
 const OneToOneMessage = require("../../models/OneToOneMessage");
+const User = require("../../models/userModels");
 
 module.exports = (socket, io, user_id) => {
   // -> to get all user to whom client chatted.
@@ -49,5 +50,55 @@ module.exports = (socket, io, user_id) => {
     });
   };
   socket.on("chat:get_OTO_chat_history", getOTOchatHistory);
+  // -------------------------------------------------------------------- //
+
+  // -> to get one to one chat history.
+  const sendOTOmsg = async ({ client_id, room_id, msg }) => {
+    // msg: -> can have
+    // {
+    //   to: "",
+    //   from: "",
+    //   type:  ["Text", "Media", "Document", "Link"],
+    //   text: "",
+    //   attachment: "",
+    // }
+
+    const conversation_history = await OneToOneMessage.findById(room_id);
+    conversation_history.lastMessage = {
+      text: msg.text,
+      seen: false,
+      createdAt: Date.now(),
+    };
+    conversation_history.messages.push(msg);
+
+    // saving doc after making changes.
+    await conversation_history.save({ new: true, validateModifiedOnly: true });
+    let reciever_id = await conversation_history.participants.find(
+      (id) => id.toString() !== client_id.toString()
+    );
+    const sender = await User.findById(client_id);
+    const reciever = await User.findById(reciever_id);
+
+    // sending information to both users.
+    io.to(reciever.socket_id).emit("chat:new_OTO_msg_recieved", {
+      room_id,
+      status: "done",
+      msg: {
+        ...msg,
+        from: {
+          _id: reciever._id,
+          firstName: reciever.firstName,
+          lastName: reciever.lastName,
+          status: reciever.status,
+        },
+      },
+    });
+    io.to(sender.socket_id).emit("chat:new_OTO_msg_sent", {
+      room_id,
+      status: "done",
+      msg,
+    });
+  };
+  socket.on("chat:send_OTO_msg", sendOTOmsg);
   // -------------------------------------------------------------------- //
 };
